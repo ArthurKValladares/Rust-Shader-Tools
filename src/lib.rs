31419ca6c32d62d0c890604f50765077f165bae1
ArthurValladares <arthurkaukal@gmail.com>
2022-11-24 01:24:09 -0800

    expose ReflectFormat

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use anyhow::Result;
 #[cfg(feature = "shader-structs")]
+pub use spirv_reflect::types::image::ReflectFormat;
+#[cfg(feature = "shader-structs")]
 use spirv_reflect::types::{image::ReflectFormat, ReflectDecorationFlags, ReflectTypeFlags};
 use spirv_reflect::ShaderModule;
 use std::{
